d434aac3c5e582e33d5dac00dd15b8d3ca7f9ec4
chai <xunxing.mxx@alibaba-inc.com>
2024-12-16 14:55:02 +0800

    feat: support dependencies

--- umd-loader.js
+++ umd-loader.js
@@ -23,22 +23,28 @@ export class UMDLoader extends Component {
     const { jsCdnAddress = [], cssCdnAddress = [] } = this.props;
     const jsUrls = typeof jsCdnAddress === 'string' ? [jsCdnAddress] : jsCdnAddress;
     const cssUrls = typeof cssCdnAddress === 'string' ? [cssCdnAddress] : cssCdnAddress;
-    return [
-      ...jsUrls.map(url => ({ type: 'JAVASCRIPT', url })),
-      ...cssUrls.map(url => ({ type: 'CSS', url })),
-    ];
+    return [...jsUrls.map((url) => ({ type: 'JAVASCRIPT', url })), ...cssUrls.map((url) => ({ type: 'CSS', url }))];
   }
 
   loadResources = () => {
+    const { npmPackages = [] } = this.props;
     const resourceUrls = this.resourceUrls;
 
     if (!resourceUrls.length) {
       this.setState({ scriptState: PromiseState.failed });
       return;
     }
+    const promises = [];
+    
+    // 加载 npm 包
+    npmPackages.forEach((pkg) => {
+      promises.push(import(pkg)); // 使用动态 import 加载 npm 包
+    });
 
-    const promises = resourceUrls.map(({ url, type }) => {
-      return type === 'CSS' ? loadStyle(url) : loadScript(url);
+    // 加载外部资源
+    resourceUrls.forEach(({ url, type }) => {
+      const loadFn = type === 'CSS' ? loadStyle : loadScript;
+      promises.push(loadFn(url));
     });
 
     Promise.all(promises)
