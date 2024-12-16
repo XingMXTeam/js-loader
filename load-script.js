d67f4e5a99d01e935ac76122de21192134546eda
chai <xunxing.mxx@alibaba-inc.com>
2024-12-16 14:52:27 +0800

    feat: opt load-script

--- load-script.js
+++ load-script.js
@@ -1,9 +1,20 @@
 
-export const loadScript = (url) => {
+export const loadScript = (url, globalName) => {
     return new Promise((resolve, reject) => {
         const script = document.createElement('script');
         script.src = url;
-        script.onload = () => resolve();
+        script.onload = () => {
+            let module = globalName ? window[globalName] : undefined;
+            if (void 0 === module) {
+                module = Object.values(window).pop();
+            }
+            // if module is promise, resolve it
+            if (module && module.then) {
+                module.then(resolve);
+            } else {
+                resolve(module);
+            }
+        };
         script.onerror = () => reject(new Error(`Failed to load script: ${url}`));
         document.body.appendChild(script);
     });
